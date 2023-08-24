90c98cda2affe29ff747ff841bd9be2836490cbc
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-24 21:53:09 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self,first_name,last_name):
+        super().__init__(self,first_name,last_name)
+        self.knowledge = []
+
     
-    def learn(self):
-        pass
+    def learn(self, skill):
+        if isinstance(skill, str):
+            self.knowledge.append(skill)
+            return self.knowledge
+        
